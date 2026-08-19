aa041e463085e28e84f7984f2a0e4db5c8c93c25
Rohit Raj <rajrohit7388@outlook.com>
2026-08-19 21:50:46 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+//int getMiddle(Node head)
+    // {
+    //     int length = 0;
+    //     Node temp = head;
+    //     while(temp != null)
+    //     {
+    //         temp = temp.next;
+    //         length++;
+    //     }
+    //     temp = head;
+    //     for(int i=1; i<=length/2; i++)
+    //     {
+    //         temp = temp.next;
+    //     }
+    //     return temp.data;
+    // }
